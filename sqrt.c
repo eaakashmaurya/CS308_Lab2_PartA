47d852e7f82bd40b32bbdd4dfd891b57b1c0a8f4
eaakashmaurya <eaakashmaurya@gmail.com>
2020-10-16 18:22:39 +0530

    Initial function written

---
+++ sqrt.c
@@ -0,0 +1,23 @@
+/* This program computes the square root of a
+positive number. */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <complex.h>
+/* Main function starts here taking input from command line*/
+int main(int argc, char* argv[]) {
+	
+	if (argc != 2) {
+		printf("Usage: sqrt input\n\n");
+		exit(-1);
+	}
+
+	int input = atoi(argv[1]);
+
+	printf(" Sqrt of %d is %f\n",input,sqrt(input));
+	printf("DEBUG: End of program. Exiting.");
+	
+	return(0);
+
+} // end main
